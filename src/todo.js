099aa594517497f9a3b76b15d8359c3237ef2b18
Brett <brettdansmith@gmail.com>
2024-06-25 12:52:47 -0700

    Initial commit for boilerplate of project

---
+++ src/todo.js
@@ -0,0 +1,9 @@
+function createTodo (title, description, dueDate, priority, project) {
+    return {
+        title: title,
+        description: description,
+        dueDate: dueDate,
+        priority: priority,
+        project: project
+    };
+}
