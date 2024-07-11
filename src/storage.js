85629045206f74e8568b7893d796878af13da078
Brett <brettdansmith@gmail.com>
2024-07-11 08:54:28 -0700

    Add storage.js and storageAvailable(type)

---
+++ src/storage.js
@@ -0,0 +1,17 @@
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
